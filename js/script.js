612dd4dbd2ec020936e98818f869015617ee3a36
simone parri <simone.parri@alten.it>
2023-12-01 18:05:00 +0100

    personalize

--- js/script.js
+++ js/script.js
@@ -389,12 +389,12 @@
         = COUNTDOWN CLOCK
     -------------------------------------------*/
     if ($("#clock").length) {
-        $('#clock').countdown('2025/12/25', function(event) {
+        $('#clock').countdown('2024/09/06', function(event) {
             var $this = $(this).html(event.strftime(''
-            + '<div class="box"><div>%D</div> <span>Days</span> </div>'
-            + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
-            + '<div class="box"><div>%M</div> <span>Mins</span> </div>'
-            + '<div class="box"><div>%S</div> <span>Secs</span> </div>'));
+            + '<div class="box"><div>%D</div> <span>Giorni</span> </div>'
+            + '<div class="box"><div>%H</div> <span>Ore</span> </div>'
+            + '<div class="box"><div>%M</div> <span>Minuti</span> </div>'
+            + '<div class="box"><div>%S</div> <span>Secondi</span> </div>'));
         });
     }
 
