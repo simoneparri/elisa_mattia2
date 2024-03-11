cae16a553a9b68f4c112cb94d100b4a6177a6da8
simone.parri <simone.parri@alten.it>
2024-03-11 17:10:07 +0100

    fix loader

--- js/script.js
+++ js/script.js
@@ -132,7 +132,7 @@
   function preloader() {
     if ($(".preloader").length) {
       $(".preloader")
-        .delay(100)
+        .delay(2000)
         .fadeOut(500, function () {
           //active wow
           wow.init();
